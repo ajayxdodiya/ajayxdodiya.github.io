41c97c49d9b21623a0968f48047f13613e41445a
Ajay Singh Dodiya <ajayxdodiya@gmail.com>
2024-03-15 17:57:06 +0530

    Inital commit

---
+++ data.js
@@ -0,0 +1,307 @@
+data = {
+    "personal": {
+      "fullName": "Ajay Dodiya",
+      "father": "Mr. Dhara Singh Dodiya",
+      "mother": "Mrs. Narmda Bai Dodiya",
+      "dob": "22 Aug, 1995",
+      "occupation": "Web Developer, Web Designer, UI Developer",
+      "maritalstatus": "Unmarried",
+      "nationality": "Indian",
+      "languages": "Hindi",
+      "about": [ "I am a creative and enthusiastic Front end developer with a strong technical skill-set, attention to detail, and",
+          " of experience. residing in Indore, India. Looking forward for great opportunities. I believe good research, astonishing concept. fresh mind, and lot of hard work leads to a successful and appealing website, And that's what I do. I regularly strengthened myself with latest design trends and latest technologies to be ahead in website industry. Always Eager to make exceptional websites."
+      ],
+      "about_web" : "I design and code beautifully simple things, and I love what I do. I am passionate about building excellent software that improves the lives of those around me. I specialize in creating software for clients ranging from individuals and small-businesses all the way to large enterprise corporations."
+    },
+    "contact": {
+      "email": "ajayxdodiya@gmail.com",
+      "website": "https://ajaydodiya.tech/",
+      "location": "Indore, MP",
+      "skype" : "https://join.skype.com/invite/udlIYfP11Tpy",
+      "github" : "https://github.com/ajayxdodiya",
+      "linkedin" : "https://linkedin.com/in/ajayxdodiya",
+      "address": {
+        "permanent": "159, Vill. Ratanpura, Teh. Badnawar, Dist. Dhar (M.P.), 454665",
+        "current": "L-27, LIG colony, Indore (M.P.), 452001"
+      }
+    },
+    "education": [
+      {
+        "title": "High School (10)",
+        "type": "10",
+        "university": "MPBSE, Bhopal, MP",
+        "passingYear": 2011,
+        "percentage": 65.33
+      },
+      {
+        "title": "Higher Secondary (10+2)",
+        "type": "12",
+        "university": "MPBSE, Bhopal, MP",
+        "passingYear": 2013,
+        "percentage": 68.2
+      },
+      {
+        "title": "Bachelor of Arts",
+        "type": "ug",
+        "university": "DAVV, Indore, MP",
+        "passingYear": 2020,
+        "percentage": 45.31
+      },
+      {
+        "title": "Master of Computer Application",
+        "type": "pg",
+        "university": "Dr. A.P.J. ABDUL KALAM UNIVERSITY, Indore",
+        "passingYear": 2022,
+        "percentage": 80
+      },
+    ],
+    "skills": [
+      { "title" : "HTML, HTML5", "percentage" : 90 },
+      { "title" : "CSS, CSS3, SCSS, Bootstrap", "percentage" : 90 },
+      { "title" : "JavaScript, jQuery, jQuery UI", "percentage" : 80 },
+      { "title" : "Angular, Typescript", "percentage" : 75 },
+      { "title" : "Php, Laravel", "percentage" : 65 },
+      { "title" : "Git, FTP, SFTP, SSH", "percentage" : 70 },
+      { "title" : "Adobe Photoshop, Adobe XD", "percentage" : 60 }
+    ],
+    "strength": [
+      "Executed lots of Projects on my own",
+      "Strong programming logic",
+      "Creative designing skills",
+      "Always eager to learn new technologies"
+    ],
+    "intrests": [
+      "Programming",
+      "Travelling",
+      "Listening Music",
+      "Learning New Technologies"
+    ],
+    "work": [
+      {
+        "company": "JSM Infosoft Pvt Ltd",
+        "location": "Indore, MP",
+        "title" : "Trainee Web Designer",
+        "date": {
+          "joining": "11 Mar, 2016",
+          "relieving": "31 Dec, 2016",
+          "total": "9 Months"
+        }
+      },
+      {
+        "company": "Oswal Computers & Consultants",
+        "location": "Indore, MP",
+        "title" : "Web Designer",
+        "date": {
+          "joining": "05 Jan, 2017",
+          "relieving": "30 Apr, 2018",
+          "total": "1 Yr 3 Months"
+        }
+      },
+      {
+        "company": "Maven Cluster Softwares Pvt Ltd",
+        "location": "Indore, MP",
+        "title" : "Sr. Front End Developer",
+        "date": {
+          "joining": "14 May, 2018",
+          "relieving": "23 Apr, 2021",
+          "total": "2 Yrs 11 Months"
+        }
+      },
+      {
+        "company": "Gatesix Technologies India Pvt. Ltd",
+        "location": "Indore, MP",
+        "title" : "UI Team Lead, SR. FRONT END DEVELOPER",
+        "date": {
+          "joining": "3 May, 2021",
+          "relieving": null,
+          "total": null
+        }
+      }
+    ],
+    "work_projects": [
+      {
+        "title": "Twittimer",
+        "role": "Front End Developer",
+        "company": "JSM Infosoft Pvt Ltd, Indore",
+        "url": "https://twittimer.com/",
+        "tech": ["HTML","CSS"],
+        "disabled": false
+      },
+      {
+        "title": "E-Aangan",
+        "role": "Front End Developer",
+        "company": "Oswal Computers & Consultants, Indore",
+        "url": "http://www.eaangan.com/",
+        "tech": ["HTML","CSS","Bootstrap"],
+        "disabled": true
+      },
+      {
+        "title": "Indore Development Authority",
+        "role": "Front End Developer",
+        "company": "Oswal Computers & Consultants, Indore",
+        "url": "https://www.idaindore.org/",
+        "tech": ["HTML","CSS","Bootstrap"],
+        "disabled": true
+      },
+      {
+        "title": "Ajmer Municipal Corporation",
+        "role": "Front End Developer",
+        "company": "Oswal Computers & Consultants, Indore",
+        "url": "http://www.ajmermc.org",
+       "tech": ["HTML","CSS","Bootstrap"],
+        "disabled": true
+      },
+      {
+        "title": "Bikaner Municipal Corporation",
+        "role": "Front End Developer",
+        "company": "Oswal Computers & Consultants, Indore",
+        "url": "http://www.bikanermc.org",
+       "tech": ["HTML","CSS","Bootstrap"],
+        "disabled": true
+      },
+      {
+        "title": "Jaipur Municipal Corporation",
+        "role": "Front End Developer",
+        "company": "Oswal Computers & Consultants, Indore",
+        "url": "http://www.jaipurmc.org",
+        "tech": ["HTML","CSS","Bootstrap"],
+        "disabled": true
+      },
+      {
+        "title": "Jodhpur Muncipal Corporation",
+        "role": "Front End Developer",
+        "company": "Oswal Computers & Consultants, Indore",
+        "url": "http://www.jodhpurmc.org",
+        "tech": ["HTML","CSS","Bootstrap"],
+        "disabled": true
+      },
+      {
+        "title": "Udaipur Municipal Corporation",
+        "role": "Front End Developer",
+        "company": "Oswal Computers & Consultants, Indore",
+        "url": "http://www.udaipurmc.org",
+        "tech": ["HTML","CSS","Bootstrap"],
+        "disabled": true
+      },
+      {
+        "title": "Unduit Wireless",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://www.unduitwireless.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","JavaScript","jQuery"],
+        "disabled": false
+      },
+      {
+        "title": "MMI Direct",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://mmi-direct.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","JavaScript","jQuery"],
+        "disabled": false
+      },
+      {
+        "title": "Callnovo",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://callnovo.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","JavaScript","jQuery"],
+        "disabled": false
+      },
+      {
+        "title": "50 Languages",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://www.50languages.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","JavaScript","jQuery"],
+        "disabled": false
+      },
+      {
+        "title": "Brown & Krippner",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://www.brownandkrippner.com.au/",
+        "tech": ["HTML","CSS","JavaScript","Bootstrap","jQuery"],
+        "disabled": false
+      },
+      {
+        "title": "Pay's Air Service",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://www.paysairservice.com.au/",
+        "tech": ["HTML","CSS","JavaScript","Bootstrap","jQuery"],
+        "disabled": false
+      },
+      {
+        "title": "ConsultingWhiz",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://www.consultingwhiz.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","JavaScript","jQuery"],
+        "disabled": false
+      },
+      {
+        "title": "Get Your Nest",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://www.getyournest.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","JavaScript","jQuery"],
+        "disabled": false
+      },
+      {
+        "title": "EXuME",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://exume.io/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","Angular","Typescript"],
+        "disabled": false
+      },
+      {
+        "title": "Redgate Portfolio Manager",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://www.portfoliobenefitmanager.com/login",
+        "tech": ["HTML","CSS","SASS","Bootstrap","Angular","Typescript"],
+        "disabled": false
+      },
+      {
+        "title": "Woffer Technologies",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "https://wooffertechnologies.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","Angular","Typescript"],
+        "disabled": false
+      },
+      {
+        "title": "Ziofly",
+        "role": "Front End Developer",
+        "company": "Maven Cluster Softwares Pvt Ltd, Indore",
+        "url": "http://ziofly.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","Angular","Typescript"],
+        "disabled": false
+      },
+      {
+        "title": "Icrest Models",
+        "role": "Front End Developer",
+        "company": "Self",
+        "url": "http://icrest.herokuapp.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","Javascript","Php"],
+        "disabled": false
+      },
+      {
+        "title": "Nuspine",
+        "role": "Front End Developer",
+        "company": "Gatesix Technologies India Pvt. Ltd, Indore",
+        "url": "https://app.nuspinechiropractic.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","Angular","Typescript"],
+        "disabled": false
+      },
+      {
+        "title": "Poolbrain",
+        "role": "Front End Developer",
+        "company": "Gatesix Technologies India Pvt. Ltd, Indore",
+        "url": "http://dashboard.poolbrain.com/",
+        "tech": ["HTML","CSS","SASS","Bootstrap","Angular","Javascript"],
+        "disabled": false
+      }
+    ]
+  };
